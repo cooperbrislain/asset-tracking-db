206fda17d5110e75015b4afbf686876d7407fb6b
Cooper Brislain <cbrislain@gmail.com>
2018-05-27 02:19:49 -0700

    logging

--- asset.php
+++ asset.php
@@ -7,9 +7,9 @@ require_once('lib/functions.php');
 require_once('lib/actions.php');
 
 $serial = $_REQUEST['serial'];
-
+error_log($serial);
 $id = find_or_create($mysqli, $serial);
-
+error_log($id);
 $asset = new Asset($mysqli, $id);
 
 echo $asset->get_json();
