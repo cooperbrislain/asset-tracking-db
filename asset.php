c41e7cc170457033f4b5ab22871059f65f08fe8e
Cooper Brislain <cbrislain@gmail.com>
2018-05-27 01:40:18 -0700

    added asset web hook

--- asset.php
+++ asset.php
@@ -5,3 +5,11 @@ require_once('mysqli.php');
 require_once('class/class_asset.php');
 require_once('lib/functions.php');
 require_once('lib/actions.php');
+
+$serial = $_REQUEST['serial'];
+
+$id = find_or_create($nysqli, $serial);
+
+$asset = new Asset($id);
+
+echo $asset->get_json();
