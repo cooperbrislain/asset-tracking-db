e54fb90da6a61d735abf89b3e9268baffd2c6e3a
Cooper Brislain <cbrislain@gmail.com>
2018-05-28 10:57:56 -0700

    assetS

--- actions/checkin.php
+++ actions/checkin.php
@@ -2,6 +2,6 @@
 function checkin($assets) {
     global $mqtt_client;
     log_event($assets, 'checked in');
-    mqtt_notify( 'leds/test/checkin', $asset[0]->descriptor->spec_id);
-    echo $asset->get_json();
+    mqtt_notify( 'leds/test/checkin', $assets[0]->descriptor->spec_id);
+    echo $assets[0]->get_json();
 }
