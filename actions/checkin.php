74ed644079d7e35ba6b3285328da6b93b7d98c3a
Cooper Brislain <cbrislain@gmail.com>
2018-05-27 19:14:57 -0700

    pass array

--- actions/checkin.php
+++ actions/checkin.php
@@ -3,6 +3,6 @@ function checkin($db, $ids) {
     global $mqtt_client;
     $id = $ids[0];
     $asset = new Asset($db, $id);
-    log_event($db, $id, 'checked in');
+    log_event($db, array($id), 'checked in');
     mqtt_notify($mqtt_client, $asset->get_json());
 }
