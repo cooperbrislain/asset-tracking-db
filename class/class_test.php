44b71a1ab517e8ef7a6f68884734e0d447497ee2
Cooper Brislain <cbrislain@gmail.com>
2018-05-28 02:59:38 -0700

    more things

---
+++ class/class_test.php
@@ -0,0 +1,8 @@
+<?php
+
+class Test {
+    public $id;
+    public $instructions;
+    public $description;
+    public $name;
+}
---
+++ class/class_testresult.php
@@ -0,0 +1,16 @@
+<?php
+
+class TestResult {
+    public $id;
+    public $test_id;
+    public $asset_id;
+    public $result;
+
+    public function get_test() {
+
+    }
+
+    public function get_asset() {
+
+    }
+}
